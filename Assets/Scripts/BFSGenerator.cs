1d047af9b1ff0a9a7c4e602fb255e7a5e69e56db
Dawid Wdowiak <dawidwdowiak@hotmail.com>
2019-09-19 22:09:46 +0200

    PoF BFS maze genration

---
+++ Assets/Scripts/BFSGenerator.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+
+public class BFSGenerator
+{
+    private int size_x, size_y;
+
+    public enum EWall
+    {
+        north = 0x1,
+        east = 0x2,
+        south = 0x4,
+        west = 0x8
+    };
+
+    private int[] walls;
+
+    public BFSGenerator(int size_x, int size_y)
+    {
+        if (size_x <= 0 || size_y <= 0)
+        {
+            throw new ArgumentException("Size of the maze is invalid");
+        }
+
+        this.size_x = size_x;
+        this.size_y = size_y;
+    }
+
+    public void GenerateMaze()
+    {
+        InitializeMaze();
+        RemoveWalls();
+
+        int x = 0;
+    }
+
+    public int GetWall(int index)
+    {
+        if(index < 0 || index >= walls.Length)
+        {
+            throw new IndexOutOfRangeException("Out of range...");
+        }
+
+        return walls[index];
+    }
+
+    public int GetWall(int x, int y)
+    {
+        // todo bound check
+        return GetWall(XYToIndex(x, y));
+    }
+
+    private void InitializeMaze()
+    {
+        walls = new int[size_x * size_y];
+        for (int i = 0; i < walls.Length; ++i)
+        {
+            walls[i] = (int)(EWall.east | EWall.north | EWall.south | EWall.west);
+        }
+    }
+
+    private void RemoveWalls()
+    {
+        Stack<int> backtracing_stack = new Stack<int>();
+        bool[] visited = new bool[size_x * size_y];
+
+        backtracing_stack.Push(0);
+        visited[0] = true;
+
+        Dictionary<Vector2Int, EWall> neighbours_directions = new Dictionary<Vector2Int, EWall> {
+            { new Vector2Int(-1, 0), EWall.north },
+            { new Vector2Int(0, 1), EWall.east },
+            { new Vector2Int(1, 0), EWall.south },
+            { new Vector2Int(0, -1), EWall.west } };
+        while (backtracing_stack.Count != 0)
+        {
+            Vector2Int current_pos = IndexToXY(backtracing_stack.Peek());
+
+            List<int> unvisited_neighbours = new List<int>();
+            foreach(var neighbour_direction in neighbours_directions)
+            {
+                Vector2Int neighbour_pos = current_pos + neighbour_direction.Key;
+                int? neighbour_index = GetNeighbourIndex(neighbour_pos.x, neighbour_pos.y);
+
+                if(neighbour_index != null && !visited[neighbour_index.Value])
+                {
+                    unvisited_neighbours.Add(neighbour_index.Value);
+                }
+            }
+
+            if (unvisited_neighbours.Count != 0)
+            {
+                int neighbour_index = unvisited_neighbours[UnityEngine.Random.Range(0, unvisited_neighbours.Count)];
+                Vector2Int neighbour_pos = IndexToXY(neighbour_index);
+
+                EWall wall_direction;
+                neighbours_directions.TryGetValue(neighbour_pos - current_pos, out wall_direction);
+                walls[backtracing_stack.Peek()] &= ~(int)wall_direction;
+
+                neighbours_directions.TryGetValue((neighbour_pos - current_pos) * (-1), out wall_direction);
+                walls[neighbour_index] &= ~(int)wall_direction;
+
+                backtracing_stack.Push(neighbour_index);
+                visited[neighbour_index] = true;
+            }
+            else
+            {
+                backtracing_stack.Pop();
+            }
+
+        }
+    }
+
+    int? GetNeighbourIndex(int x, int y)
+    {
+        if (x < 0 || x >= size_x || y < 0 || y >= size_y)
+        {
+            return null;
+        }
+
+        return XYToIndex(x, y);
+    }
+
+    public int XYToIndex(int x, int y)
+    {
+        return x * size_y + y;
+    }
+
+    public Vector2Int IndexToXY(int index)
+    {
+        int x = index / size_y;
+        int y = index % size_y;
+
+        return new Vector2Int(x, y);
+    }
+}
---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public int size_x, size_y;
+
+    public GameObject floor_prefab, wall_prefab;
+    public float grid_size, wall_thickness;
+
+    void Start()
+    {
+        BFSGenerator gen = new BFSGenerator(size_x, size_y);
+        gen.GenerateMaze();
+
+        SpawnMaze(gen);
+    }
+
+    private void SpawnMaze(BFSGenerator gen)
+    {
+        SpawnFloor();
+        SpawnWalls(gen);
+    }
+
+    private void SpawnFloor()
+    {
+        var floor = Instantiate(floor_prefab, new Vector3(grid_size * size_x / 2.0f, 0, grid_size * size_y / 2.0f), Quaternion.identity);
+        floor.transform.localScale = new Vector3(grid_size * size_x, grid_size, grid_size * size_y);
+    }
+
+    private void SpawnWalls(BFSGenerator gen)
+    {
+        for (int x = 0; x < size_x; ++x)
+        {
+            for (int y = 0; y < size_y; ++y)
+            {
+                int walls = gen.GetWall(x, y);
+                if ((walls & (int)BFSGenerator.EWall.north) == 1)
+                {
+                    GameObject wall = Instantiate(wall_prefab, new Vector3(x, 1, y + grid_size / 2.0f), Quaternion.identity);
+                    wall.name = "North Wall[" + x + "][" + y + "]";
+                    wall.transform.localScale = new Vector3(wall_thickness, 1, grid_size);
+                }
+
+                if ((walls & (int)BFSGenerator.EWall.east) == (int)BFSGenerator.EWall.east)
+                {
+                    GameObject wall = Instantiate(wall_prefab, new Vector3(x + grid_size / 2.0f, 1, y + grid_size), Quaternion.identity);
+                    wall.name = "East Wall[" + x + "][" + y + "]";
+                    wall.transform.localScale = new Vector3(grid_size, 1, wall_thickness);
+                }
+
+                if ((walls & (int)BFSGenerator.EWall.south) == (int)BFSGenerator.EWall.south)
+                {
+                    GameObject wall = Instantiate(wall_prefab, new Vector3(x + grid_size, 1, y + grid_size / 2.0f), Quaternion.identity);
+                    wall.name = "South Wall[" + x + "][" + y + "]";
+                    wall.transform.localScale = new Vector3(wall_thickness, 1, grid_size);
+                }
+
+                if ((walls & (int)BFSGenerator.EWall.west) == (int)BFSGenerator.EWall.west)
+                {
+                    GameObject wall = Instantiate(wall_prefab, new Vector3(x + grid_size / 2.0f, 1, y), Quaternion.identity);
+                    wall.name = "West Wall[" + x + "][" + y + "]";
+                    wall.transform.localScale = new Vector3(grid_size, 1, wall_thickness);
+                }
+
+            }
+        }
+    }
+}
